917f6e578fcc85a4966b061bd5d2ebdf428d4293
FabioGrosso <37393451+FabioGrosso@users.noreply.github.com>
2024-06-28 18:10:40 -0600

    Update compressor.sh

--- compressor.sh
+++ compressor.sh
@@ -1,11 +1,4 @@
-# cd SZ_SLE
-# mkdir install
-# mkdir build
-# cd build
-# cmake -DCMAKE_INSTALL_PREFIX:PATH=$AMRIC_HOME/SZ_SLE/install -DBUILD_H5Z_FILTER=ON ..
-# export SZ_SLE_HOME=$AMRIC_HOME/SZ_SLE/install
-# make -j 8
-# make install 
+
 
 cd $AMRIC_HOME
 cd SZ3
@@ -48,7 +41,6 @@ make -j 8
 make install
 cd $AMRIC_HOME 
 
-echo export SZ_SLE_HOME=$AMRIC_HOME/SZ_SLE/install >> ~/.bashrc
 echo export SZ3_HOME=$AMRIC_HOME/SZ3/install >> ~/.bashrc
 echo export ORI_SZ3_HOME=$AMRIC_HOME/orisz3/install >> ~/.bashrc
 echo "# end of AMRIC env" >> ~/.bashrc
